4da52d228ab8385d04a901ddc0d90ac8136726cb
nileshchandra07 <56809007+nileshchandra07@users.noreply.github.com>
2022-07-26 09:06:06 +1200

    Create nuBrowseScroll.js
    vertical scroll fix for larger number of records

---
+++ experimental/nuBrowseScroll.js
@@ -0,0 +1,11 @@
+if (nuFormType() == 'browse') {
+if (!nuIsIframe()) {
+
+$('#nuRECORD').css({'width': '100vw', 'height': '85vh', 'overflow-x': 'auto', 'overflow-y': 'auto' });
+$('#nuBreadcrumbHolder').css({ 'width': '100vw', 'display': 'flex', 'flex': '1', 'flex-flow': 'row wrap' });
+$('#nuActionHolder').css({ 'width': '100vw' });
+document.body.style.overflow = 'hidden';
+}
+} else {
+document.body.style.overflow = 'visible';
+}

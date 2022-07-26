004ec8f98283b40bfe27a5e4d8e6cec631ddac13
nileshchandra07 <56809007+nileshchandra07@users.noreply.github.com>
2022-07-26 13:35:12 +1200

    fixed browse header height and top position

--- experimental/nuBrowseScroll.js
+++ experimental/nuBrowseScroll.js
@@ -1,28 +1,29 @@
-
-        if (nuFormType() == 'browse') {
+if (nuFormType() == 'browse') {
             if (!nuIsIframe()) {
                 
                 $('#nuRECORD').css({'width': '100vw', 'height': '85vh', 'overflow-x': 'auto', 'overflow-y': 'auto' });
                 $('#nuBreadcrumbHolder').css({ 'width': '100vw', 'display': 'flex', 'flex': '1', 'flex-flow': 'row wrap' });
                 $('#nuActionHolder').css({ 'width': '100vw' });
-                 $('.nuBrowseTitle').wrapAll('<div id= "btitle"></div>');
+                 $('.nuBrowseTitle, .nuBrowseTitleMultiline').wrapAll('<div id= "btitle"></div>');
         //adding sticky nuBrowseTitle      
 $('#nuRECORD').bind("scroll", function() {
   
        var scrollLeft = $('#nuRECORD') .scrollLeft();
           var scrollTop = $('#nuRECORD').scrollTop();
-       
+       //$('.nuBrowseTitle').each(function() {
             if (scrollTop >= 0 && scrollLeft >= 0) {
                
-                $('#btitle').css({ 'z-index': '95','position': 'fixed', 'left':-scrollLeft + 'px'});
-            
+                $('#btitle').css({ 'z-index': '95','position': 'fixed', 'left':-scrollLeft + 'px' });
+                $('.nuBrowseTitle').css({'top':'0', 'height':'28px'});
+                $('.nuBrowseTitleMultiline ').css({'top':'0', 'height':'48px'});
+             
              
            }else{
                $('#btitle').css({ 'z-index': '0','position': 'absolute' });
-                
+                $('.nuBrowseTitle,.nuBrowseTitleMultiline ').css({'top':'3px'});
             }
             
-            
+      // });       
            
 });       
                 

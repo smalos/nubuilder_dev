1f114aadddd7aeb03788129eb407dc829d0f69e7
kev1n <45562597+smalos@users.noreply.github.com>
2022-08-17 02:33:10 +0200

    Updated experimental\nuformclass.js
    collect all children within a container class

--- experimental/nuformclass.js
+++ experimental/nuformclass.js
@@ -747,7 +747,7 @@ class nuFormObject {
 
 			});
 
-			THIS.children().children('[data-nu-data]').each(function () {
+			THIS.children('.container').find('[data-nu-data]').each(function () {
 				
 				let children = cThis.subformCollectRecordChildren(this, sf, chk, deleteAll, V, E, F, C);
 				chk = children.chk;
